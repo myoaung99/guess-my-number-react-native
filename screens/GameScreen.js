a4aba38b3719701d90c8ce77ba54a55923b6d7e2
MyoMyintAung <myomyintaung064@gmail.com>
2022-07-19 19:52:18 +0630

    navigate to Game Screen With React useState

---
+++ screens/GameScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function GameScreen(props) {
+    return (
+        <View>
+            <Text>This is the game screen</Text>
+        </View>
+    );
+}
+
+export default GameScreen;
